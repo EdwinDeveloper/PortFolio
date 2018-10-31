64b1838078a67f796fa5439c4e68b2d05aa7f9cb
Edwin <edwin_020193@hotmail.com>
2018-10-31 17:26:22 -0600

    Side bar avance

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
-var Edwin = "Edwin Giovanni Pérez Robles";
-var nombre=document.createTextNode(Edwin);
-var h1Nombre=document.createElement('h1');
-var header=document.getElementById('header');
-h1Nombre.classList.add('Presentacion');
-h1Nombre.appendChild(nombre);
-header.appendChild(h1Nombre);
+$(document).ready(()=>{
+    printName();
+    getOption();
+});
+const getOption = () =>{
+    $('.menu-options-container div').on('click',(e)=>{
+        let optionSelected = $(e.target).text();
+        console.log(optionSelected);
+    });
+}
+const printName = () =>{
+    var Edwin = "Edwin Giovanni Pérez Robles";
+    var nombre=document.createTextNode(Edwin);
+    var h1Nombre=document.createElement('h1');
+    var header=document.getElementById('header');
+    h1Nombre.classList.add('Presentacion');
+    h1Nombre.appendChild(nombre);
+    header.appendChild(h1Nombre);
+}
+
